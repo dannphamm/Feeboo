2489f525976fe69f4695bf6e51395004426be7ab
Dung Pham <dungpham@Dungs-MacBook-Pro.local>
2017-05-23 23:26:11 -0400

    2nd commit

--- FeeBoo/ViewController.swift
+++ FeeBoo/ViewController.swift
@@ -7,19 +7,20 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func loginTapped(_ sender: Any) {
+    
     }
-
 
 }
 
